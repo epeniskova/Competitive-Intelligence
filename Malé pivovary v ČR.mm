cddad5c85ea9c32a0afb5941a9809035c3c01d2b
epeniskova <69634307+epeniskova@users.noreply.github.com>
2020-08-18 15:30:38 +0200

    Add files via upload

---
+++ Malé pivovary v ČR.mm
@@ -0,0 +1,402 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="CI pro mal&#xe9; pivovary" LOCALIZED_STYLE_REF="default" FOLDED="false" ID="ID_1661180400" CREATED="1578045096929" MODIFIED="1578062403189" COLOR="#000000" BACKGROUND_COLOR="#ccccff" NodeVisibilityConfiguration="SHOW_HIDDEN_NODES">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#ffffff" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1661180400" STARTINCLINATION="40;0;" ENDINCLINATION="40;45;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#ffffff" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1661180400" STARTINCLINATION="40;0;" ENDINCLINATION="40;45;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#ffffff" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1661180400" STARTINCLINATION="40;0;" ENDINCLINATION="40;45;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font SIZE="20" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<hook NAME="MapStyle" background="#ffffff">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="11" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="2 Key Information Questions" POSITION="right" ID="ID_764639395" CREATED="1578045633883" MODIFIED="1578059692350" HGAP_QUANTITY="-120.24999599903833 pt" VSHIFT_QUANTITY="236.2499929592015 pt" BACKGROUND_COLOR="#ff9999">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+<node TEXT="Jak&#xe9; produkty jsou nab&#xed;zeny?" ID="ID_764440288" CREATED="1578052391391" MODIFIED="1578063882898" HGAP_QUANTITY="13.25000002235174 pt" VSHIFT_QUANTITY="-38.2499988600612 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://monitoring-anopress-cz.zdroje.vse.cz/Anopress#">https://monitoring-anopress-cz.zdroje.vse.cz/Anopress#; Vyhled&#225;v&#225;n&#237; &quot;druhy piva&quot;</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Kdo je na&#x161;&#xed; konkurenc&#xed;?" ID="ID_477654413" CREATED="1578052539386" MODIFIED="1578063846868">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Gnosus
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak&#xe9; jsou podnikatelsk&#xe9; trendy v odv&#x11b;tv&#xed;?" ID="ID_684284440" CREATED="1578052564377" MODIFIED="1578064033367" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="26.99999919533732 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-f5b859c8-7fff-b275-fce6-12fec4fff882" style="font-weight: normal"><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #000000; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#000000">https://www.euro.cz/byznys/minipivovary-cesko-ma-novou-turistickou-atrakci-13953</font></span></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak&#xfd; je trend prodeje to&#x10d;en&#xfd;ch piv?" ID="ID_817736518" CREATED="1578052596305" MODIFIED="1578064443050" HGAP_QUANTITY="16.249999932944778 pt" VSHIFT_QUANTITY="-28.499999150633837 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-6d5252bc-7fff-456f-f106-639ece8947a8" style="font-weight: normal"><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #000000; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#000000">&#160;https://www.euro.cz/byznys/minipivovary-cesko-ma-novou-turistickou-atrakci-13953</font></span></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Kolik mal&#xfd;ch pivovar&#x16f; existuje?" LOCALIZED_STYLE_REF="AutomaticLayout.level,8" ID="ID_271817145" CREATED="1578052676955" MODIFIED="1578691555000" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="-20.999999374151248 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-441242f3-7fff-f2a4-1d7a-4b7330e1e4bd" style="font-weight: normal"><a href="http://amsp.cz/cesi-berou-utokem-male-pivovary/" style="text-decoration: none"><u><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #0563c1; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: underline; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#0563c1">http://amsp.cz/cesi-berou-utokem-male-pivovary/</font></span></u></a></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak&#xe1; je c&#xed;lov&#xe1; skupina na&#x161;ich z&#xe1;kazn&#xed;k&#x16f;?" ID="ID_1026420155" CREATED="1578058529339" MODIFIED="1578064520586" VSHIFT_QUANTITY="14.999999552965178 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-b5eff920-7fff-8ba6-5bf5-50bfcfbc845d" style="font-weight: normal"><a href="http://amsp.cz/cesi-berou-utokem-male-pivovary/" style="text-decoration: none"><u><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #0563c1; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: underline; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#0563c1">http://amsp.cz/cesi-berou-utokem-male-pivovary/</font></span></u></a></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak&#xfd;m legislativn&#xed;m omezen&#xed;m je nutn&#xe9; &#x10d;elit?" ID="ID_403823465" CREATED="1578058601121" MODIFIED="1578064563650" VSHIFT_QUANTITY="11.999999642372142 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-4b4ddf1f-7fff-2fd2-5772-4210398fbc6b" style="font-weight: normal"><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #1155cc; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: underline; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#1155cc"><u>https://www.celnisprava.cz/cz/dane/spotrebni-dane/ostatni/Stranky/default.aspx</u></font></span></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak&#xe1; je v&#xfd;&#x161;e spot&#x159;ebn&#xed; dan&#x11b; z piva?" ID="ID_271200808" CREATED="1578058730319" MODIFIED="1578064570826" HGAP_QUANTITY="15.499999955296518 pt" VSHIFT_QUANTITY="14.999999552965178 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-4b4ddf1f-7fff-2fd2-5772-4210398fbc6b" style="font-weight: normal"><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #1155cc; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: underline; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#1155cc"><u>https://www.celnisprava.cz/cz/dane/spotrebni-dane/ostatni/Stranky/default.aspx</u></font></span></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Je aktu&#xe1;ln&#xed; situace na trhu p&#x159;&#xed;zniv&#xe1; pro rozvoj odv&#x11b;tv&#xed;?" ID="ID_710125893" CREATED="1578058780998" MODIFIED="1578064591964" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="20.999999374151248 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-a7854a0b-7fff-a578-4f90-b973b39d99b5" style="font-weight: normal"><a href="https://www.kurzy.cz/makroekonomika/hdp/" style="text-decoration: none"><u><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #1155cc; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: underline; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#1155cc">https://www.kurzy.cz/makroekonomika/hdp/</font></span></u></a></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak se prom&#xed;tne aktu&#xe1;ln&#xed; stav nezam&#x11b;stnanosti do odv&#x11b;tv&#xed;?" ID="ID_1309194704" CREATED="1578059167161" MODIFIED="1578064613413" HGAP_QUANTITY="13.25000002235174 pt" VSHIFT_QUANTITY="21.74999935179951 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-3badcd55-7fff-f6b5-2da8-fe3f861c76e4" style="font-weight: normal"><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #1155cc; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: underline; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#1155cc"><u>https://www.kurzy.cz/makroekonomika/nezamestnanost/?imakroGraphFrom=1.1.2009</u></font></span></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Existuje prostor pro mo&#x17e;nou implementaci nov&#xfd;ch technologi&#xed;?" ID="ID_1468123794" CREATED="1578059319292" MODIFIED="1578064640193" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="8.249999754130847 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://ego.ihned.cz/c1-66699470-cesi-uz-nepostavaji-u-vycepu-ale-s-velkym-hrncem-doma-v-kuchyni-a-vari-si-vlastni-pivo">https://ego.ihned.cz/c1-66699470-cesi-uz-nepostavaji-u-vycepu-ale-s-velkym-hrncem-doma-v-kuchyni-a-vari-si-vlastni-pivo</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak&#xe9; jsou p&#x159;&#xed;le&#x17e;itosti a co n&#xe1;m mohou p&#x159;in&#xe9;st?" ID="ID_1727507375" CREATED="1578059431018" MODIFIED="1578064671129" VSHIFT_QUANTITY="25.4999992400408 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      SWOT anal&#253;za
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak&#xe9; jsou hrozby a jak n&#xe1;s mohou ohrozit?" ID="ID_995385858" CREATED="1578059453585" MODIFIED="1578064681012" VSHIFT_QUANTITY="10.499999687075624 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      SWOT anal&#253;za
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Jak&#xe1; je vlastnick&#xe1; struktura na&#x161;&#xed; konkurence?" ID="ID_1166439544" CREATED="1578059598067" MODIFIED="1578064688354" VSHIFT_QUANTITY="10.499999687075624 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Gnosus
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="1 Key Intelligence Topics" POSITION="left" ID="ID_1156155082" CREATED="1578045681765" MODIFIED="1578062385012" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="-261.249991796911 pt" VSHIFT_QUANTITY="-247.49999262392544 pt" COLOR="#333333" BACKGROUND_COLOR="#9999ff">
+<font SIZE="16"/>
+<edge COLOR="#0000ff"/>
+<node TEXT="Charakteristika produkt&#x16f; a slu&#x17e;eb" ID="ID_841456100" CREATED="1578046951775" MODIFIED="1578062158426" HGAP_QUANTITY="-395.4999877959494 pt" VSHIFT_QUANTITY="-18.74999944120647 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ofici&#225;ln&#237; webov&#233; str&#225;nky konkurent&#367;.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Identifikace kl&#xed;&#x10d;ov&#xfd;ch konkurent&#x16f; a jejich vlastnick&#xe9; struktury" ID="ID_922476950" CREATED="1578047008240" MODIFIED="1578052129752" HGAP_QUANTITY="-487.7499850466852 pt" VSHIFT_QUANTITY="-16.499999508261695 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://ceskepivo-ceskezlato.cz/seznam-pivovaru/vyber/?rg=cr&amp;so=s5">https://ceskepivo-ceskezlato.cz/seznam-pivovaru/vyber/?rg=cr&amp;so=s5 </a>
+    </p>
+    <p>
+      <a href="https://ceskepivo-ceskezlato.cz/seznam-pivovaru/vyber/?rg=cr&amp;so=s5">Gnosus</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Nov&#xe9; trendy v podnikatelsk&#xe9;m prost&#x159;ed&#xed;" ID="ID_656715173" CREATED="1578047014429" MODIFIED="1578052049941" HGAP_QUANTITY="-426.24998687952797 pt" VSHIFT_QUANTITY="-12.749999620020407 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-fcdb2933-7fff-7121-7f64-b05846b68953" style="font-weight: normal"><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #000000; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#000000">https://www.euro.cz/byznys/minirevoluce-male-pivovary-se-v-tichosti-slucuji-do-skupin-1360463</font></span></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Kategorizace z&#xe1;kazn&#xed;k&#x16f;" ID="ID_1014582321" CREATED="1578047015102" MODIFIED="1578062162143" HGAP_QUANTITY="-342.24998938292293 pt" VSHIFT_QUANTITY="5.999999821186071 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-9d9480ab-7fff-1c0b-82ce-74a213b0a73f" style="font-weight: normal"><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #000000; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: none; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#000000">Pr&#367;zkum srpen 2017 pro AMSP &#268;R, Jak vn&#237;m&#225; &#269;esk&#225; populace mal&#233; pivovary? http://amsp.cz/uploads/TZ_2017_08_17_Ipsos_minipivovary_pro_AMSP_media_bez_metody_sberu.pdf</font></span></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="P&#x159;&#xed;le&#x17e;itosti a hrozby" ID="ID_1030599738" CREATED="1578047044929" MODIFIED="1578062172855" HGAP_QUANTITY="-319.74999005347513 pt" VSHIFT_QUANTITY="15.749999530613437 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      SWOT anal&#253;za
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Legislativn&#xed; faktory" ID="ID_1865921829" CREATED="1578047056286" MODIFIED="1578052374992" HGAP_QUANTITY="-313.7499902322891 pt" VSHIFT_QUANTITY="35.249998949468164 pt">
+<font SIZE="12"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b charset="utf-8" id="docs-internal-guid-3893bd21-7fff-fec6-8395-dec031d7ccf9" style="font-weight: normal"><a href="https://www.finance.cz/527687-nizsi-dph-u-piva-holicky/" style="text-decoration: none"><u><span style="font-size: 11pt; font-family: Calibri,sans-serif; color: #1155cc; font-weight: 400; font-style: normal; font-variant: normal; text-decoration: underline; vertical-align: baseline; white-space: pre-wrap"><font size="11pt" face="Calibri,sans-serif" color="#1155cc">https://www.finance.cz/527687-nizsi-dph-u-piva-holicky/</font></span></u></a></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
